4c0d915507a2dddbdee2dc3d6e5cbd9bf0e088cd
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-07-22 14:06:18 +0200

    add navigation items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
+import { BiHomeCircle, BiHash, BiUser } from 'react-icons/bi';
+import { HiEnvelope } from 'react-icons/hi2';
+import { BsBell } from 'react-icons/bs';
+import { FaBookmark } from 'react-icons/fa';
+import Link from 'next/link';
+
+const NAVIGATION_ITEMS = [
+	{
+		title: 'Home',
+		icon: BiHomeCircle,
+	},
+	{
+		title: 'Explore',
+		icon: BiHash,
+	},
+	{
+		title: 'Notifications',
+		icon: BsBell,
+	},
+	{
+		title: 'Messages',
+		icon: HiEnvelope,
+	},
+	{
+		title: 'Bookmarks',
+		icon: FaBookmark,
+	},
+	{
+		title: 'Profile',
+		icon: BiUser,
+	},
+];
 const Home = () => {
 	return (
 		<div className='w-full h-full flex justify-center items-center relative'>
 			<div className='max-w-screen-lg w-full h-full flex relative'>
 				<section className='fixed w-72 flex flex-col'>
-					left sliderbar
+					{NAVIGATION_ITEMS.map((item) => (
+						<Link
+							className='bg-white/50 flex items-center justify-center space-x-2 rounded-3xl p-4'
+							key={item.title}
+							href={`/${item.title.toLowerCase()}`}
+						>
+							<div>
+								<item.icon />
+							</div>
+							<div>{item.title}</div>
+						</Link>
+					))}
 				</section>
 				<main>Home timeline</main>
 				<section>right sliderbar</section>
