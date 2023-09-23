79df8913c7d025f5a8b80c9ad4922ace31d52240
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-09-23 12:24:37 +0200

    add loading

---
+++ app/loading.jsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+	return (
+		<div className='w-full flex-center'>
+			<div>Loading ....</div>
+		</div>
+	);
+};
+
+export default Loading;
--- app/page.jsx
+++ app/page.jsx
@@ -14,6 +14,7 @@ const Home = () => {
 				Promptopia is an open-source AI prompting tool for modern world
 				to discover, create and share creative prompts
 			</p>
+
 			<Feed />
 		</section>
 	);
